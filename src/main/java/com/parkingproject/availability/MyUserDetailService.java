feaad81aafb0d3b668e0fba13f9244cbcc45d6c7
Sean-Long-ATL <jackrat67@gmail.com>
2020-04-19 16:59:18 -0700

    spring security

---
+++ src/main/java/com/parkingproject/availability/MyUserDetailService.java
@@ -0,0 +1,24 @@
+package com.parkingproject.availability;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MyUserDetailService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUserName(username);
+
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + username));
+        return user.map(MyUserDetails::new).get();
+    }
+}
---
+++ src/main/java/com/parkingproject/availability/securityConfiguration.java
@@ -0,0 +1,39 @@
+package com.parkingproject.availability;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class securityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        super.configure(auth);
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       http.authorizeRequests()
+               .antMatchers("/admin").hasRole("ADMIN")
+               .antMatchers("/").permitAll()
+               .and().formLogin();
+    }
+ @Bean
+    public PasswordEncoder getPasswordEncoder(){ return NoOpPasswordEncoder.getInstance(); }
+    //this is clear text encoding fix later
+}
